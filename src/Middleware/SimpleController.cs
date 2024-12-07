fd3f06cc95dd41a5485d5b094b155a3628614c67
Stiven Villegas <svillegasc@protonmail.com>
2024-12-07 09:46:53 -0500

    Default webapi project using regular MVC controllers

---
+++ src/Middleware/SimpleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware;
+
+[ApiController]
+[Route("api/simple")]
+public class SimpleController : ControllerBase
+{
+    public record UserRequest(string Username, string Password);
+
+    [HttpPost]
+    public IActionResult Post([FromBody] UserRequest request)
+    {
+        Console.WriteLine($"User: {request.Username}, Password: {request.Password}");
+        return Ok();
+    }
+}
